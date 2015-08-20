682c0e64bbc53b4936a9442288d2497eb9e92a81
Marius Herring <mariusherring@gmail.com>
2015-08-20 07:52:34 +0200

    Added more toggle

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -1,9 +1,9 @@
 
-;; Move windows, even in org-mode
-(global-set-key (kbd "<s-right>") 'windmove-right)
-(global-set-key (kbd "<s-left>") 'windmove-left)
-(global-set-key (kbd "<s-up>") 'windmove-up)
-(global-set-key (kbd "<s-down>") 'windmove-down)
+;; Window switching
+(windmove-default-keybindings) ;; Shift+direction
+(global-set-key (kbd "C-x -") 'toggle-window-split)
+(global-set-key (kbd "C-x C--") 'rotate-windows)
+(global-unset-key (kbd "C-x C-+")) ;; don't zoom like this
 
 
 ;; Expand region (increases selected region by semantic units)
